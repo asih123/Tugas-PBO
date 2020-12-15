5c0b43c24165d0abd0723039312ff1b2fbd64f5c
asih123 <74700286+asih123@users.noreply.github.com>
2020-12-15 15:34:18 +0700

    Add files via upload

---
+++ Property.php
@@ -0,0 +1,11 @@
+<?php
+   //cara penulisan class dan property OOP PHP - @pulung_coding
+   class kelinci{          //class kelinci
+     var $warna;           //property warna
+     var $jenis;           //property jenis
+     var $ukuran;          //property ukuran
+         
+     
+   }
+   
+ ?>
