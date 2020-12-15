69ad4e38a7eacdf7fe428e7e8f4970586d9af197
asih123 <74700286+asih123@users.noreply.github.com>
2020-12-15 15:33:11 +0700

    Add files via upload

---
+++ Method.php
@@ -0,0 +1,24 @@
+<?php
+   //cara penulisan class dan property OOP PHP - @pulung_coding
+class kelinci{
+  
+  //property OOP
+ var $warna;
+ var $jenis;
+ var $ukuran;
+ 
+ //method OOP
+ 
+  function melompat() {
+    //isi method
+    
+  
+  }  
+    function berhenti() {
+      //isi method
+      
+    
+  }
+}
+
+ ?>
